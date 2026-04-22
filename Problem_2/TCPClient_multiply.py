2957fe4f9fdd9f6abfee498c85ff625b15875a87
Emily <66390405+rushingwater13@users.noreply.github.com>
2026-04-22 16:10:06 +0000

    add templetes for TCP problem

---
+++ Problem_2/TCPClient_multiply.py
@@ -0,0 +1,15 @@
+from socket import *
+# Create a TCP socket
+clientSocket = socket(AF_INET, SOCK_STREAM)
+# Connect to a server process using a tuple of IP and port
+serverIP = '127.0.0.1'
+serverPort = 12345
+serverAddress = (serverIP, serverPort)
+clientSocket.connect(serverAddress)
+# Send a byte string to the client socket
+message = input('Input lowercase sentence:')
+clientSocket.send(message.encode("utf-8"))
+# Receive a byte string from the client socket
+modifiedMessageBytes = clientSocket.recv(1024)
+print('From Server: ', modifiedMessageBytes.decode("utf-8"))
+clientSocket.close()
