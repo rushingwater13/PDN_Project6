373e1d9ed4ed7cf0673ef019acbac56f9aef351d
Emily <66390405+rushingwater13@users.noreply.github.com>
2026-04-22 16:09:01 +0000

    fiddling with regex for mrjob

--- Problem_1/find_longest_words_MRJob.py
+++ Problem_1/find_longest_words_MRJob.py
@@ -1,11 +1,15 @@
 from mrjob.job import MRJob
+import re
+
+WORD_RE = re.compile(r"[A-Za-z'-]+")
+#WORD_RE = re.compile(r"[\w']+")
 
 
 class MRLongestWords(MRJob):
 
         def mapper(self, _, line):
             # for each word, sort by first letter
-            for word in line.split():
+            for word in WORD_RE.findall(line):
                 word = clean(word)
 
                 if word and word[0].isalpha():
@@ -29,7 +33,10 @@ class MRLongestWords(MRJob):
         def reducer(self, letter, words):
             # find the longest length
             words = list(words)
-            maxLength = max(len(word) for word in words)
+            maxLength = 0
+            for word in words:
+                if len(word) > maxLength:
+                    maxLength = len(word)
 
             # find the words with that longest length
             longest = []
@@ -37,12 +44,12 @@ class MRLongestWords(MRJob):
                  if len(word) == maxLength:
                       longest.append(word)
                     
-            longest = set(longest)
+            longest = list(set(longest))
             yield letter, longest
 
 
 def clean(word):
-    return word.strip(".,!?:;\"'()[]{}_").lower()
+    return word.strip(".,!?:;\"'()[]{}_—”“").lower()
 
 
 if __name__ == '__main__':
